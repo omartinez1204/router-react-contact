887c22f36d75bde852db49a7b160c032f1f3a58b
Omar Martinez <omar1204@gmail.com>
2023-12-08 10:13:27 -0600

    Home App

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './css/style.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
 )
